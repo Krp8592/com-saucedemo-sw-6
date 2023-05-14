e5ec29df6f0c81046c6e9fd6a72427ad956fa813
Krp8592 <patelkashyap8555@gmail.com>
2023-05-14 20:51:27 +0100

    2

--- src/test/java/com/saucedemo/runners/SanityRunners.java
+++ src/test/java/com/saucedemo/runners/SanityRunners.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/java/resources/featurefile",
-        glue = "com/nopecommerce/demo",
+        glue = "com/saucedemo",
         plugin = {"pretty", "html:target/cucumber-report/cucumber.html",
                 "com.cucumber.listener.ExtentCucumberFormatter:target/Extent_Reports/report.html",
                 "json:target/RunCuke/cucumber.json"},
