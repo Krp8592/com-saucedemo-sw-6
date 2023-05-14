61e271d19f1bd479b04d77ad15dacbcfad609a71
Krp8592 <patelkashyap8555@gmail.com>
2023-05-14 20:13:30 +0100

    Week-15-saturday-Homework

---
+++ src/main/java/com/saucedemo/pages/MainPage.java
@@ -0,0 +1,28 @@
+package com.saucedemo.pages;
+
+import com.saucedemo.utility.Utility;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
+
+public class MainPage extends Utility {
+
+    private static final Logger log = LogManager.getLogger(MainPage.class.getName());
+
+    public MainPage(){
+        PageFactory.initElements(driver, this);
+    }
+    By listOfProducts = By.xpath("//div[@class='inventory_item']");
+
+    //List of displayed web elements
+    public int numberOfProductsDisplayed() {
+        log.info("Number of products displayed : ");
+        List<WebElement> numberOfProducts = driver.findElements(listOfProducts);
+        return numberOfProducts.size();
+    }
+}
+
